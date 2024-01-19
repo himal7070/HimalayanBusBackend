46567ff5cc8196d070ec240cc916dc41ca18105a
himal <356260@student.fontys.nl>
2024-01-19 22:41:04 +0100

    removed unused imports

--- src/main/java/com/himalayanbus/controller/AuthController.java
+++ src/main/java/com/himalayanbus/controller/AuthController.java
@@ -8,9 +8,6 @@ import com.himalayanbus.service.IAuthService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
 
